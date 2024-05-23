e427ef5fbb960cd2c91989a38a24e60330b4401c
erinyooey <69409957+erinyooey@users.noreply.github.com>
2024-05-23 10:39:33 -0700

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,17 @@
+// Pseudo Code
+
+// 1, Link html to js.
+// 2. Create a variable let called message for the user. 
+// 3. Create each variable for each code number. Perform the calculation.
+// 4. Create an alert message for pop up dialog.
+
+// Message for the user to see. 
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+
+// Perform calculation for each number. The combination for the vault.
+let num1 = 2*5
+let num2 = 80/2
+let num3 = 40-1
+
+// Creates a pop-up message with the combination. 
+alert(`${message}: ${num1}-${num2}-${num3}`)
